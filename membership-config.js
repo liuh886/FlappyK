b29c1cf994663df2cdd39a10f6cd9b4554493507
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2026-08-05 23:13:12 +0800

    config: keep billing actions gated until secrets are ready

--- membership-config.js
+++ membership-config.js
@@ -10,8 +10,8 @@
         supabaseUrl: 'https://blgwlycfcwvsupmqyqwn.supabase.co',
         supabasePublishableKey: 'sb_publishable_n1Va-c_alpkQ0zNuJYUaxA_J0u68RVW',
         entitlementCode: 'flappyk.pro',
-        checkoutFunctionUrl: 'https://blgwlycfcwvsupmqyqwn.supabase.co/functions/v1/create-checkout-session',
-        portalFunctionUrl: 'https://blgwlycfcwvsupmqyqwn.supabase.co/functions/v1/create-portal-session',
+        checkoutFunctionUrl: '',
+        portalFunctionUrl: '',
         redirectUrl: 'https://liuh886.github.io/FlappyK/',
     });
 })();
