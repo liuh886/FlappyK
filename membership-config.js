7a9f32fb53526ed8d0e47e0b6603d14f9459c509
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2026-08-14 01:45:32 +0800

    chore: load referral v3 (#100)

--- membership-config.js
+++ membership-config.js
@@ -57,14 +57,14 @@
             const stylesheet = document.createElement('link');
             stylesheet.id = 'hao-product-referral-styles';
             stylesheet.rel = 'stylesheet';
-            stylesheet.href = 'https://liuh886.github.io/admin/shared/product-referral.css?v=1';
+            stylesheet.href = 'https://liuh886.github.io/admin/shared/product-referral.css?v=3';
             document.head.appendChild(stylesheet);
         }
 
         if (!document.getElementById('hao-product-referral-script')) {
             const script = document.createElement('script');
             script.id = 'hao-product-referral-script';
-            script.src = 'https://liuh886.github.io/admin/shared/product-referral.js?v=1';
+            script.src = 'https://liuh886.github.io/admin/shared/product-referral.js?v=3';
             script.async = true;
             document.head.appendChild(script);
         }
